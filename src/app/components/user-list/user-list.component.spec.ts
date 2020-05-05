8d97927a8379610fe3874409833a0f4b67a48ed0
The Bimmy's <bimmys@air-di-the.home>
2020-05-05 14:15:20 +0100

    added unit test

--- src/app/components/user-list/user-list.component.spec.ts
+++ src/app/components/user-list/user-list.component.spec.ts
@@ -1,14 +1,31 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { HttpClientModule } from '@angular/common/http';
+import { UserService } from '../../services/user.service';
 import { UserListComponent } from './user-list.component';
+import { mockResponse } from '../../mocks/dataApi.mock';
 
 describe('UserListComponent', () => {
   let component: UserListComponent;
   let fixture: ComponentFixture<UserListComponent>;
+  let compiled: HTMLHtmlElement;
+  const users = [{
+    id: '1',
+    firstName: 'Luke',
+    lastName: 'Skywalker',
+    enabled: true,
+  },
+  {
+    id: '2',
+    firstName: 'Leia',
+    lastName: 'Skywalker',
+    enabled: false,
+  }];
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ UserListComponent ]
+      declarations: [ UserListComponent ],
+      providers: [UserService],
+      imports: [HttpClientModule]
     })
     .compileComponents();
   }));
@@ -17,9 +34,37 @@ describe('UserListComponent', () => {
     fixture = TestBed.createComponent(UserListComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+    component.users = users;
+    compiled = fixture.nativeElement;
   });
 
-  it('should create', () => {
+  it('should create the component', () => {
     expect(component).toBeTruthy();
   });
+
+  describe('should render ', () => {
+
+    it('the user list with right number of users', () => {
+      fixture.detectChanges();
+      const chipsEl = compiled.querySelectorAll('.userList__item');
+      expect(chipsEl.length).toBe(2);
+    });
+
+    it('the chip with a different style if user is enabled', () => {
+      fixture.detectChanges();
+      const chipsEl = compiled.querySelectorAll('.userList__item');
+      expect(chipsEl[0].classList.contains('userList__item--enabled')).toBe(users[0].enabled);
+      expect(chipsEl[1].classList.contains('userList__item--enabled')).toBe(users[1].enabled);
+    });
+
+    it('the chip with a delete icon if user is enabled', () => {
+      fixture.detectChanges();
+      const chipsEl = compiled.querySelectorAll('.userList__item');
+      expect(chipsEl[0].querySelector('mat-icon')).toBeTruthy();
+      expect(chipsEl[0].querySelector('mat-icon').textContent).toBe('cancel');
+      expect(chipsEl[1].querySelector('mat-icon')).toBeNull();
+    });
+  })
+
+  
 });
--- src/app/services/user.service.spec.ts
+++ src/app/services/user.service.spec.ts
@@ -1,16 +1,95 @@
 import { TestBed } from '@angular/core/testing';
+import {
+  HttpClientTestingModule,
+  HttpTestingController
+} from '@angular/common/http/testing';
 
 import { UserService } from './user.service';
 
+import { mockResponse } from '../mocks/dataApi.mock';
+import { ApiResponse } from '../interfaces/api-response.interface';
+import { map } from 'rxjs/operators';
+import { User } from '../interfaces/user.interface';
+
 describe('UserService', () => {
   let service: UserService;
+  let httpMock: HttpTestingController;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [UserService]
+    });
+
     service = TestBed.inject(UserService);
+    httpMock = TestBed.get(HttpTestingController);
   });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
+  afterEach(() => {
+    httpMock.verify();
+  });
+  
+
+  it('should get all JSON data object', () => {
+    service.getJSON().subscribe((data: ApiResponse) => {
+      expect(data.list).toBeTruthy();
+      expect(data.list.pagination).toBeTruthy();
+      expect(data.list.entries).toBeTruthy();
+      expect(data).toBe(mockResponse);
+    });
+
+    const req = httpMock.expectOne(service.dataJSON, 'call to api');
+    expect(req.request.method).toBe('GET');
+
+    req.flush(mockResponse);
+
+    httpMock.verify();
+  });
+
+  it('should get the users from API response', () => {
+    service.getJSON().pipe(
+      map(service.getUsers)
+    )
+    .subscribe((users: User[]) => {
+      expect(users.length).toBe(3);
+      expect(users[0].firstName).toBe('Luke Skywalker');
+      expect(users[0].enabled).toBe(true);
+      expect(users[0].company).toBeTruthy();
+      expect(users[1].firstName).toBe('Leia Skywalker');
+      expect(users[1].enabled).toBe(true);
+      expect(users[2].firstName).toBe('Han Solo');
+      expect(users[2].enabled).toBe(false);
+    });
+
+    const req = httpMock.expectOne(service.dataJSON, 'call to api');
+    expect(req.request.method).toBe('GET');
+
+    req.flush(mockResponse);
+
+    httpMock.verify();
+  });
+
+  it('should delete an user if exist', () => {
+    let users = service.getUsers(mockResponse);
+    const userToDelete = users[0];
+
+    expect(users.length).toBe(3);
+    users = service.deleteUser(users, userToDelete);
+    expect(users.length).toBe(2);
+    expect(users.find(user => user.id === userToDelete.id)).toBeUndefined();
+  });
+
+  it('should return the same users array if trying to delete a not-existing user', () => {
+    let users = service.getUsers(mockResponse);
+    const notExistingUser: User = {
+      firstName: 'Unicorn',
+      id: '11212121',
+      emailNotificationsEnabled: false,
+      enabled: false
+    };
+
+    expect(users.length).toBe(3);
+    users = service.deleteUser(users, notExistingUser);
+    expect(users.length).toBe(3);
   });
 });
